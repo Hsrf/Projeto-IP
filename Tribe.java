ac6f51fa504e8c888f30ecce9510936d5b0aabcf
LuanaPorciuncula <38366811+LuanaPorciuncula@users.noreply.github.com>
2018-06-13 10:52:34 -0300

    Vikings em progresso :)

---
+++ Tribe.java
@@ -0,0 +1,8 @@
+package comoTreinarDragao;
+
+public class Tribe {
+	public String getTribe () {
+		return "batata";
+	}
+}
+//Sas merda aq é pra não aparecer erros, mas pode apagar e adicionar a classe de vdd :)
