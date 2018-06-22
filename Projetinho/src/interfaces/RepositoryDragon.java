7b4aefe63810a9b038ed2424ca742a7c0ace9857
gabrielaheimer <38387611+gabrielaheimer@users.noreply.github.com>
2018-06-22 08:14:11 -0300

    Oi bbs - Repositorios dragon prontos

--- Projetinho/src/interfaces/RepositoryDragon.java
+++ Projetinho/src/interfaces/RepositoryDragon.java
@@ -4,7 +4,9 @@ import classesBasicas.Dragon;
 
 public interface RepositoryDragon {
 	public void insert(Dragon dragon);
-	public Dragon search (Dragon dragon);
+	public Dragon search (String name);
 	public void update(Dragon dragon, String name);
 	public void remove(Dragon dragon);
+	public boolean existsDragon (Dragon dragon);
+	public boolean existsName (String name);
 }
