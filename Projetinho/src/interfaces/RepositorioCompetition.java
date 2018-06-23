7100a0045e5f639d066ee8c9954b2cb4dc08e0e4
Hsrf <38447080+Hsrf@users.noreply.github.com>
2018-06-23 20:25:23 -0300

    Delete RepositorioCompetition.java

--- Projetinho/src/interfaces/RepositorioCompetition.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-import classesBasicas.Competition;
-
-public interface RepositorioCompetition {
-	public void inserir(Competition competition);
-	public void procurar(Competition competition);
-	public void atualizar(Competition competition);
-	public void remover(Competition competition);
-}
