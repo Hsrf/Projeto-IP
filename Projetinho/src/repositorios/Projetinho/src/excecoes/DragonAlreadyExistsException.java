2525c0d75edc927d0180fc1fac5fead4309fb4e7
Hsrf <38447080+Hsrf@users.noreply.github.com>
2018-06-25 01:43:26 -0300

    Delete DragonAlreadyExistsException.java

--- Projetinho/src/repositorios/Projetinho/src/excecoes/DragonAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package excecoes;
-
-public class DragonAlreadyExistsException extends Exception{
-	public DragonAlreadyExistsException (String name) {
-		super ("O dragão" + name + "já existe");
-	}
-}
