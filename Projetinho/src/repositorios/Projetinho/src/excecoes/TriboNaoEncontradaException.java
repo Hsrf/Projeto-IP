7da52ae5ea291723acf84ef592765805f17bb171
Hsrf <38447080+Hsrf@users.noreply.github.com>
2018-06-25 01:43:55 -0300

    Delete TriboNaoEncontradaException.java

--- Projetinho/src/repositorios/Projetinho/src/excecoes/TriboNaoEncontradaException.java
+++
@@ -1,7 +0,0 @@
-package excecoes;
-
-public class TriboNaoEncontradaException extends Exception {
-	public TriboNaoEncontradaException(String nome) {
-        super("A tribo " + nome + " nao foi encontrada");
-    }
-}
