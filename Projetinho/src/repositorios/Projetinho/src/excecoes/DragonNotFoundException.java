eff3c31fa449761b5221217687f74125d1223795
Hsrf <38447080+Hsrf@users.noreply.github.com>
2018-06-25 01:43:36 -0300

    Delete DragonNotFoundException.java

--- Projetinho/src/repositorios/Projetinho/src/excecoes/DragonNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package excecoes;
-
-import classesBasicas.Dragon;
-
-public class DragonNotFoundException extends Exception {
-	public DragonNotFoundException (String name) {
-		super ("O dragão" + name + "não foi encontrado");
-	}
-}
