5b510572f51380ad901fd9e412a6a3735118b271
Hsrf <38447080+Hsrf@users.noreply.github.com>
2018-06-25 01:43:47 -0300

    Delete TriboJaCadastradaException.java

--- Projetinho/src/repositorios/Projetinho/src/excecoes/TriboJaCadastradaException.java
+++
@@ -1,7 +0,0 @@
-package excecoes;
-
-public class TriboJaCadastradaException extends Exception {
-	public TriboJaCadastradaException(String nome) {
-        super("A tribo " + nome + " ja foi cadastrada");
-    }
-}
