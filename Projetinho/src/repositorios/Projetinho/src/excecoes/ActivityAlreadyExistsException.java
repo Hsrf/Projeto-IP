437bbd50c77bac6e52571d618e1e3915af5eafc3
Hsrf <38447080+Hsrf@users.noreply.github.com>
2018-06-25 01:43:14 -0300

    Delete ActivityAlreadyExistsException.java

--- Projetinho/src/repositorios/Projetinho/src/excecoes/ActivityAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package excecoes;
-
-public class ActivityAlreadyExistsException extends Exception{
-	public ActivityAlreadyExistsException (int id) {
-		super ("A atividade de código: "+ id + " já existe!");
-	}
-}
