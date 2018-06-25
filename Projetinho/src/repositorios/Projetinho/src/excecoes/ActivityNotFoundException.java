6f86cb3b5dafce82263da3a1d205139f0025c10a
Hsrf <38447080+Hsrf@users.noreply.github.com>
2018-06-25 01:43:21 -0300

    Delete ActivityNotFoundException.java

--- Projetinho/src/repositorios/Projetinho/src/excecoes/ActivityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package excecoes;
-
-public class ActivityNotFoundException extends Exception{
-	public ActivityNotFoundException (int id) {
-		super ("A atividade de código: "+ id +" não foi encontrada!");
-	}
-}
